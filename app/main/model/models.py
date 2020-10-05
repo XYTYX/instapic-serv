f1421e9df1442e9b4d3de7efbd33dcefec8d2263
Aidan Cheng <aidan.cheng94@gmail.com>
2020-10-05 14:36:13 +0800

    DB

--- app/main/model/models.py
+++ app/main/model/models.py
@@ -75,7 +75,7 @@ class Post(db.Model):
     __tablename__ = "posts"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     created_on = db.Column(db.DateTime, nullable=False)
     text = db.Column(db.String(255))
     public_id = db.Column(db.String(100), unique=True)
@@ -87,7 +87,7 @@ class Image(db.Model):
     __tablename__ = "images"
 
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
-    post_id = db.Column(db.Integer, db.ForeignKey('post.id'), nullable=False)
+    post_id = db.Column(db.Integer, db.ForeignKey('posts.id'), nullable=False)
     created_on = db.Column(db.DateTime, nullable=False)
     path = db.Column(db.String(255), unique=True, nullable=False)
 
