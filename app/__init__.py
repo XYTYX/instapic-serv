13323cc568ba4df7a7363c66ddb654be4ea1dad7
Aidan Cheng <aidan.cheng94@gmail.com>
2020-10-05 13:22:19 +0800

    WIP 2

--- app/__init__.py
+++ app/__init__.py
@@ -16,33 +16,3 @@ api = Api(blueprint,
 api.add_namespace(user_ns, path='/v1/user')
 api.add_namespace(auth_ns, path='/v1/auth')
 api.add_namespace(post_ns, path='/v1/post')
-
-# from flask import Flask
-# from flask_sqlalchemy import SQLAlchemy
-
-# app = Flask(__name__)
-# app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
-
-# db = SQLAlchemy(app)
-
-# from models import User
-
-# @app.route('/add/')
-# def webhook():
-#     name = "ram"
-#     email = "ram@ram.com"
-#     u = User(id = id, nickname = name, email = email)
-#     print("user created", u)
-#     db.session.add(u)
-#     db.session.commit()
-#     return "user created"
-
-# @app.route('/delete/')
-# def delete():
-#     u = User.query.get(i)
-#     db.session.delete(u)
-#     db.session.commit()
-#     return "user deleted"
-
-# if __name__ == '__main__':
-#     app.run()
