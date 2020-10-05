7f41839d7f751279919aee67ddbd40b20989ed3e
Aidan Cheng <aidan.cheng94@gmail.com>
2020-10-05 13:08:47 +0800

    WIP

--- app/main/config.py
+++ app/main/config.py
@@ -1,8 +1,6 @@
 import os
 
-# uncomment the line below for postgres database url from environment variable
 basedir = os.path.abspath(os.path.dirname(__file__))
-
 
 class Config:
     SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
