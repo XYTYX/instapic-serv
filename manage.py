17d52e38f8d1d4dc618756bd3e770feba9bc64f5
Aidan Cheng <aidan.cheng94@gmail.com>
2020-10-08 16:19:20 +0800

    Revert

--- manage.py
+++ manage.py
@@ -19,8 +19,8 @@ migrate = Migrate(app, db, render_as_batch=True, compare_type=True)
 
 manager.add_command('db', MigrateCommand)
 
-@app.route('/<path>')
-def index(path):
+@app.route('/')
+def index():
     return app.send_static_file('index.html') 
 
 @manager.command
