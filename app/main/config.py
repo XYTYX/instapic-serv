6c9f4972c88de54188fb397f1be55c39f5820149
Aidan Cheng <aidan.cheng94@gmail.com>
2020-10-10 01:40:54 +0800

    secret_key

--- app/main/config.py
+++ app/main/config.py
@@ -3,7 +3,7 @@ import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY')
+    SECRET_KEY = os.getenv('SECRET_KEY', os.urandom(32))
     DEBUG = False
 
 
