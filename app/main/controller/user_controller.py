c287080a236676c81897079fcb55f008ceb77245
Aidan Cheng <aidan.cheng94@gmail.com>
2020-10-12 15:43:33 +0800

    Getting rid of admin_token

--- app/main/controller/user_controller.py
+++ app/main/controller/user_controller.py
@@ -12,7 +12,6 @@ _user = UserDto.user
 @api.route('/')
 class UserList(Resource):
     @api.doc('list of registered users')
-    @admin_token_required
     @api.marshal_list_with(_user, envelope='users')
     def get(self):
         """List all registered users"""
