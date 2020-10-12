5918eb1c8ced54c0c71c4bba25174cddeca18d28
Aidan Cheng <aidan.cheng94@gmail.com>
2020-10-12 20:38:13 +0800

    building better sort by user

--- app/main/controller/user_controller.py
+++ app/main/controller/user_controller.py
@@ -12,7 +12,7 @@ _user = UserDto.user
 @api.route('/')
 class UserList(Resource):
     @api.doc('list of registered users')
-    @api.marshal_list_with(_user, envelope='users')
+    @api.marshal_list_with(_user)
     def get(self):
         """List all registered users"""
         return get_all_users()
