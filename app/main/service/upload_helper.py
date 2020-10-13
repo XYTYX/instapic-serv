1e553df5f19d6633799c03669d9f9e3666f6e9b2
Aidan Cheng <aidan.cheng94@gmail.com>
2020-10-13 12:55:40 +0800

    Making test

--- app/main/service/upload_helper.py
+++ app/main/service/upload_helper.py
@@ -2,14 +2,21 @@ import boto3, botocore
 from flask import current_app
 import botocore.session
 from botocore.stub import Stubber
+from moto import mock_s3
+
+def get_client():
+    if current_app.config['TESTING']:
+        return mock_s3
+    else:
+        return boto3.client(
+            "s3",
+            aws_access_key_id=current_app.config['AWS_KEY'],
+            aws_secret_access_key=current_app.config['AWS_SECRET']
+        )
 
 
 def upload_file_to_s3(file, filename, acl="public-read"):
-    s3 = boto3.client(
-        "s3",
-        aws_access_key_id=current_app.config['AWS_KEY'],
-        aws_secret_access_key=current_app.config['AWS_SECRET']
-    )
+    s3 = get_client()
 
     bucket_name = current_app.config['S3_BUCKET']
 
